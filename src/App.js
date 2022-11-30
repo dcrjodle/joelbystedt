cbd9bab811aea41a2106b23976ddf5a54b01fc7b
Joel Bystedt <joel.bystedt@gmail.com>
2022-11-30 11:40:09 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div className='headerText'>
+          <n>Hi! 👋</n>
+          <p>I'm Joel Bystedt, a design addicted, passionate, outside the box-thinker, ambitious, tech enthusiastic, and creative guy looking for a career.</p>
+          <a>Resume</a>
+        </div>
+    </div>
+  );
+}
+
+export default App;
